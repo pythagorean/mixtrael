95e89fa0b561b16bdea141daaa9fdbc5bb0cd42d
Michael Goldman <mike.goldman@allstreets.org>
2023-12-30 19:37:02 -0700

    Tighten up

--- app.py
+++ app.py
@@ -8,10 +8,7 @@ pn.extension(design="material")
 
 
 def get_claude_conversations(claude):
-    conversations = []
-    for conversation in claude.list_all_conversations():
-        conversations.append((conversation['uuid'], conversation['name']))
-    return reversed(conversations)
+    return [(conv['uuid'], conv['name']) for conv in reversed(claude.list_all_conversations())]
 
 
 def get_mixtral_conversations(mixtral):
@@ -51,11 +48,13 @@ def get_chat_interface(callback, placeholder) -> pn.chat.ChatInterface:
     )
 
 
+def create_client_selection_config(i):
+    return pn.widgets.Select(name=f'Client {i}', options=['Echo', 'Mixtral', 'Claude'])
+
+
 number_of_conversations = pn.widgets.IntSlider(
     name='Number of conversations', value=2, start=2, end=8)
-
 configure_button = pn.widgets.Button(name="Configure")
-
 
 start_panel = pn.Column(
     number_of_conversations,
@@ -63,128 +62,81 @@ start_panel = pn.Column(
 )
 
 
-def create_client_selection_config(i):
-    client = pn.widgets.Select(name=f'Client {i}', options=[
-                               'Echo', 'Mixtral', 'Claude'])
-    return client
+def configure_client_panel(client_type, clients):
+    if clients.count(client_type) > 1:
+        raise ValueError(
+            f'Only one {client_type} client can currently be selected')
+    client = get_hugchat_client() if client_type == 'Mixtral' else get_claude_client()
+    conversations = get_mixtral_conversations(
+        client) if client_type == 'Mixtral' else get_claude_conversations(client)
+
+    conversation_options = {'0. Start a new conversation': 'New'}
+    conversation_dict = {}
+    for i, (conv_id, title) in enumerate(conversations, 1):
+        conv_id_str = str(conv_id)
+        conversation_dict[conv_id_str] = conv_id
+        conversation_options[f"{i}. {title}"] = conv_id_str
+
+    return pn.widgets.Select(
+        options=conversation_options,
+        value=None,
+        name=f"Select a {client_type} conversation"
+    ), conversation_dict
 
 
 def configured_start_panel(event):
     num_conversations = number_of_conversations.value
     client_selection_configs = [create_client_selection_config(
-        i) for i in range(1, num_conversations+1)]
+        i) for i in range(1, num_conversations + 1)]
     select_clients_panel = pn.Column(
-        *client_selection_configs,
-        configure_button
-    )
+        *client_selection_configs, configure_button)
 
     def selected_clients_panel(event):
         app.clear()
         clients = [client.value for client in client_selection_configs]
-        if 'Claude' in clients:
-            if clients.count('Claude') > 1:
-                raise ValueError(
-                    'Only one Claude client can currently be selected')
-            anthropic_claude = get_claude_client()
-            claude_conversations = get_claude_conversations(anthropic_claude)
-            anthropic_claude_current_conversation = None
-            claude_conversation_selections = {
-                '0. Start a new conversation': 'New'}
-            for i, (conv_id, title) in enumerate(claude_conversations, 1):
-                claude_conversation_selections[f"{i}. {title}"] = conv_id
-            select_claude_conversation = pn.widgets.Select(
-                options=claude_conversation_selections,
-                value=None,
-                name="Select a conversation"
-            )
-        if 'Mixtral' in clients:
-            if clients.count('Mixtral') > 1:
-                raise ValueError(
-                    'Only one Mixtral client can currently be selected')
-            hugchat_mixtral = get_hugchat_client()
-            mixtral_conversations = get_mixtral_conversations(hugchat_mixtral)
-            hugchat_mixtral.current_conversation = None
-            mixtral_conversation_selections = {
-                '0. Start a new conversation': 'New'}
-            mixtral_conversation_dict = {}
-            for i, (conv_id, title) in enumerate(mixtral_conversations, 1):
-                conv_id_str = str(conv_id)
-                mixtral_conversation_dict[conv_id_str] = conv_id
-                mixtral_conversation_selections[f"{i}. {title}"] = conv_id_str
-            select_mixtral_conversation = pn.widgets.Select(
-                options=mixtral_conversation_selections,
-                value=None,
-                name="Select a conversation"
-            )
+        selected_conversations = {}
+
+        for client_type in clients:
+            if client_type == 'Echo':
+                continue
+
+            select_conversation, conversation_dict = configure_client_panel(
+                client_type, clients)
+            selected_conversations[client_type] = {
+                'select': select_conversation, 'dict': conversation_dict}
 
         configuration_panel_elements = []
         for i, client in enumerate(clients):
-            if client == 'Claude':
-                configuration_panel_elements.append(
-                    f"Claude Conversation {i+1}")
-                configuration_panel_elements.append(select_claude_conversation)
-            elif client == 'Mixtral':
-                configuration_panel_elements.append(
-                    f"Mixtral Conversation {i+1}")
-                configuration_panel_elements.append(
-                    select_mixtral_conversation)
+            if client == 'Echo':
+                continue
+            select_info = selected_conversations[client]
+            configuration_panel_elements.extend(
+                [f"Conversation {i+1}", select_info['select']])
 
         configuration_panel = pn.Column(
-            *configuration_panel_elements,
-            configure_button
-        )
+            *configuration_panel_elements, configure_button)
 
         def configured_panel(event):
-            selected_claude_conversation = select_claude_conversation.value
-            if selected_claude_conversation == 'New':
-                new_chat = anthropic_claude.create_new_chat()
-                anthropic_claude_current_conversation = new_chat['uuid']
-            else:
-                anthropic_claude_current_conversation = selected_claude_conversation
-
-            selected_mixtral_conversation = select_mixtral_conversation.value
-            if selected_mixtral_conversation == 'New':
-                hugchat_mixtral.new_conversation(switch_to=True)
-            else:
-                hugchat_mixtral.change_conversation(
-                    mixtral_conversation_dict[selected_mixtral_conversation])
-
-            mixtral_conversation = hugchat_mixtral.get_conversation_info()
-            mixtral_conversation_title = pn.pane.Markdown(
-                f"# Current Conversation: {mixtral_conversation.title}")
-
-            echo_chat = get_chat_interface(
-                echo_callback, placeholder="Text to echo")
-
-            claude_chat = get_chat_interface(
-                lambda contents, user, instance: anthropic_callback(
-                    contents, user, instance, anthropic_claude, anthropic_claude_current_conversation),
-                placeholder="Text to Claude!")
-
-            mixtral_chat = get_chat_interface(
-                lambda contents, user, instance: hugchat_callback(
-                    contents, user, instance, hugchat_mixtral),
-                placeholder="Text to Mixtral!")
-
-            buffer = pn.widgets.TextAreaInput(
-                placeholder="Buffer space", auto_grow=True, rows=1)
-            copy_button = pn.widgets.Button(name='Copy to Buffer')
-            paste_button = pn.widgets.Button(name='Paste to Echo')
-            buffer_space = pn.Row(
-                buffer,
-                copy_button,
-                paste_button
-            )
-
-            main_panel = pn.Column(
-                claude_chat,
-                buffer_space,
-                mixtral_conversation_title,
-                mixtral_chat
-            )
-
             app.clear()
-            app.append(main_panel)
+            for client_type, select_info in selected_conversations.items():
+                selected_conversation = select_info['select'].value
+                if selected_conversation == 'New':
+                    if client_type == 'Claude':
+                        new_chat = get_claude_client().create_new_chat()
+                        current_conversation = new_chat['uuid']
+                    elif client_type == 'Mixtral':
+                        get_hugchat_client().new_conversation(switch_to=True)
+                else:
+                    current_conversation = select_info['dict'][selected_conversation]
+
+                if client_type == 'Claude':
+                    chat = get_chat_interface(lambda contents, user, instance: anthropic_callback(
+                        contents, user, instance, get_claude_client(), current_conversation), placeholder="Text to Claude!")
+                elif client_type == 'Mixtral':
+                    chat = get_chat_interface(lambda contents, user, instance: hugchat_callback(
+                        contents, user, instance, get_hugchat_client()), placeholder="Text to Mixtral!")
+
+                app.append(chat)
 
         pn.bind(configured_panel, configure_button, watch=True)
         app.append(configuration_panel)
@@ -196,6 +148,4 @@ def configured_start_panel(event):
 
 pn.bind(configured_start_panel, configure_button, watch=True)
 app = start_panel
-
-
 app.servable()
